af48715e0390fb9b276a425837d5598cd8a4c230
Dan Stetsenko <denyast@ukr.net>
2019-10-07 22:54:13 +0300

    Preparations, configs etc.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     HelloWorld
   }
@@ -18,7 +17,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
