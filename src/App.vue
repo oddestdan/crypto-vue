3893abdfcf27f0772b49f593506f666c59cd026a
Dan Stetsenko <denyast@ukr.net>
2019-10-14 23:05:05 +0300

    MVP algorithm created.

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,45 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Helvetica Now', 'Montserrat', Arial, sans-serif;
+  -webkit-text-size-adjust: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
+
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+hr {
+  box-sizing: content-box;
+  height: 0;
+  overflow: visible;
+}
+bold {
+  font-weight: bolder;
+}
+light {
+  font-weight: lighter;
+}
+.eyebrow {
+  font-size: 20px;
+}
+.-text-primary {
+  color: #39b982;
+}
+.-text-base {
+  color: #000;
+}
+.-text-error {
+  color: tomato;
+}
+.-text-gray {
+  color: rgba(0, 0, 0, 0.5);
+}
+.-shadow {
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.13);
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,144 @@
 <template>
-  <div class="home-wrapper">This is a home wrapper</div>
+  <div class="home-wrapper">
+    <div class="text-container">
+      <h2>Input message</h2>
+      <!-- <text-reader @load="inputMessage = $event"></text-reader> -->
+      <textarea rows="15" v-model="inputMessage"></textarea>
+      <h2>Key</h2>
+      <textarea rows="10" v-model="key"></textarea>
+    </div>
+    <select name="action" id="action" v-model="action">
+      <option value="Encrypt">Encrypt</option>
+      <option value="Decrypt">Decrypt</option>
+    </select>
+    <select name="cipher" id="cipher" v-model="cipher">
+      <option value="Caesar">Caesar</option>
+      <option value="Tritemius">Tritemius</option>
+    </select>
+    <button @click="applyAction" type="button" id="applyActionButton">
+      {{ action }}
+    </button>
+  </div>
 </template>
 
 <script>
-export default {};
+// import TextReader from '@/components/TextReader';
+
+export default {
+  components: {
+    // TextReader
+  },
+
+  data() {
+    return {
+      inputMessage: '',
+      action: 'Encrypt',
+      cipher: 'Caesar',
+      key: ''
+    };
+  },
+
+  methods: {
+    applyAction() {
+      switch (this.cipher) {
+        case 'Caesar':
+          this.inputMessage = this.cipherCaesar(
+            this.inputMessage,
+            Number.parseInt(this.key),
+            this.action
+          );
+          break;
+        case 'Tritemius':
+          this.inputMessage = this.cipherTritemius(
+            this.inputMessage,
+            this.key,
+            this.action
+          );
+          break;
+        default:
+          this.inputMessage = this.cipherCaesar(
+            this.inputMessage,
+            Number.parseInt(this.key),
+            this.action
+          );
+          break;
+      }
+    },
+
+    cipherCaesar(input, shift, mode) {
+      console.log(mode);
+      let modif = mode === 'Encrypt' ? 1 : -1;
+      var output = '';
+      var temp = 0;
+      var i = 0;
+
+      //step through the string 1 character at a time
+      for (i = 0; i < input.length; i++) {
+        //store the ASCII value of each character
+        temp = input.charCodeAt(i);
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+
+        // if (temp >= 65 && temp <= 122) {
+        //   temp = ((temp - 65 + shift * modif) % 58) + 65;
+        // }
+
+        if (temp >= 65 && temp <= 90) {
+          // Uppercase
+          temp = ((temp - 65 + shift * modif) % 26) + 65;
+        } else if (temp >= 97 && temp <= 122) {
+          // Lowercase
+          temp = ((temp - 97 + shift * modif) % 26) + 97;
+        } else {
+          temp = '?';
+        }
+
+        output += String.fromCharCode(temp);
+      }
+      return output;
+
+      // let sign = mode === 'Encrypt' ? 1 : -1;
+      // return input
+      //   .replace(/[^A-Z]/g, '')
+      //   .replace(/./g, a =>
+      //     String.fromCharCode(65 + ((a.charCodeAt(0) - 65 + sign * shift) % 26))
+      //   );
+
+      // Encrypting
+      // if (mode == 'Encrypt') {
+      //   return (input + '').replace(/./g, s =>
+      //     String.fromCharCode(
+      //       s.charCodeAt(0) + (s.toLowerCase() < 'n' ? key : -key)
+      //     )
+      //   );
+      // }
+      // // Decrypting
+      // else {
+      //   return (input + '').replace(/./g, s =>
+      //     String.fromCharCode(
+      //       s.charCodeAt(0) + (s.toLowerCase() < 'n' ? -key : +key)
+      //     )
+      //   );
+      // }
+    },
+    // eslint-disable-next-line no-unused-vars
+    cipherTritemius(input, key, mode) {}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .home-wrapper {
   margin-top: 60px;
+}
+textarea {
+  resize: none;
+}
+textarea {
+  box-sizing: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 5px;
+  font-size: 14px;
+  font-family: 'Helvetica Now', Montserrat, Arial, sans-serif;
 }
 </style>
